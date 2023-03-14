8ae37a677f672cf3e58bab33254d998bada17d3c
Eugene Perminov <gekauta@gmail.com>
2023-03-14 23:28:18 +0200

    11

---
+++ task2.js
@@ -0,0 +1,15 @@
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+switch (numOrStr){
+    case null:
+    console.log('вы отменили');
+    break;
+    case '':
+    console.log('Empty String');
+    break;
+    case !isNaN(numOrStr) || numOrStr :
+    console.log(' number is Ba_NaN') ;
+    break;
+    default:
+    console.log('OK!')
+}
